5bf946838c907f0adc4219a6f9fcc9aa2540ab85
Nuran Cetin <demir_nuran@hotmail.com>
2020-11-27 23:42:58 -0500

    B21 is Dedicated

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello Git Last Day");
         // B21 is excited!!!!
+        //B21 is Dedicated
     }
 }
